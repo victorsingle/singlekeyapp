68a817997fcc4c6eac362006ad935ab47a5223cd
Victor Teixeira <victor@singlejob.com.br>
2025-04-23 00:30:21 -0300

    Corrige uso de chave do Supabase no client

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseServiceRoleKey = import.meta.env.VITE_SUPABASE_SERVICE_ROLE_KEY;  // Use a chave `service_role` aqui
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseServiceRoleKey);  // Agora usando a chave `service_role`
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);

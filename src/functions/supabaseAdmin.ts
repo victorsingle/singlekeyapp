b51346b706abda67bd7faabdeac2de6b171ced8e
Victor Teixeira <victor@singlejob.com.br>
2025-04-25 20:29:20 -0300

    Correção Variávl Supabase

--- src/functions/supabaseAdmin.ts
+++ src/functions/supabaseAdmin.ts
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js';
 
 export const supabaseAdmin = createClient(
   process.env.VITE_SUPABASE_URL!,
-  process.env.VITE_SUPABASE_SERVICE_ROLE_KEY! // ⚠️ Não usar no frontend, mas aqui é função Netlify
+  process.env.SUPABASE_SERVICE_ROLE_KEY! // ⚠️ Não usar no frontend, mas aqui é função Netlify
 );

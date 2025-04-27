21d423534e99f85f90b2c2624a19c6f6e9fbe23e
Victor Teixeira <victor@singlejob.com.br>
2025-04-27 08:23:14 -0300

    Ajustes no layout de Recuperação de Senha

--- src/components/auth/ResetPassword.tsx
+++ src/components/auth/ResetPassword.tsx
@@ -41,7 +41,7 @@ export function ResetPassword() {
 
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md text-center">
+      <div className="w-full text-center">
         {/* Logo */}
         <div className="flex justify-center">
           <Target className="h-20 w-20 text-blue-600" />

b0b53449bf3d62c6f64540d789cdcdf79d0df2fb
Victor Teixeira <victor@singlejob.com.br>
2025-05-23 20:09:09 -0300

    V2 KAI Chat Interativo - Cadastra Times no Onboarding

--- src/stores/useKaiChatStore.ts
+++ src/stores/useKaiChatStore.ts
@@ -2,11 +2,19 @@ import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
 import { ParsedOKRStructure } from '../types/okr';
 
+interface TeamInput {
+  name: string;
+  description?: string;
+  organization_id: string;
+}
+
 interface KaiChatState {
   estruturaJson: ParsedOKRStructure | null;
   propostaConfirmada: boolean;
+  teamsToCreate: TeamInput[];
   setEstruturaJson: (json: ParsedOKRStructure) => void;
   setPropostaConfirmada: (valor: boolean) => void;
+  setTeamsToCreate: (teams: TeamInput[]) => void;
   resetKai: () => void;
 }
 
@@ -14,9 +22,11 @@ export const useKaiChatStore = create<KaiChatState>()(
   devtools((set) => ({
     estruturaJson: null,
     propostaConfirmada: false,
+    teamsToCreate: [],
 
     setEstruturaJson: (json) => set({ estruturaJson: json }),
     setPropostaConfirmada: (valor) => set({ propostaConfirmada: valor }),
-    resetKai: () => set({ estruturaJson: null, propostaConfirmada: false }),
+    setTeamsToCreate: (teams) => set({ teamsToCreate: teams }),
+    resetKai: () => set({ estruturaJson: null, propostaConfirmada: false, teamsToCreate: [] }),
   }))
 );
